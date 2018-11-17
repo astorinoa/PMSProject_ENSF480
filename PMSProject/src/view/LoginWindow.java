31d31aa752f86075005c88dde90c8549dc22a993
Alexa Astorino <alexa.astorino@gmail.com>
2018-11-17 11:23:27 -0700

    added login window - copypaste

---
+++ PMSProject/src/view/LoginWindow.java
@@ -0,0 +1,161 @@
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import java.awt.Font;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import javax.swing.SwingConstants;
+import javax.swing.JTextField;
+import java.awt.Color;
+import javax.swing.JPasswordField;
+import javax.swing.border.MatteBorder;
+
+import control.LoginListener;
+
+import javax.swing.border.CompoundBorder;
+
+/**
+ * Creates a panel for user login
+ * @author Alexa Astorino, Shreya Patel, Rae McPhail
+ *
+ */
+public class LoginWindow extends JFrame {
+
+	private JTextField userID;
+	private JPasswordField passwordField;
+	private JButton login;
+	private ObjectInputStream objIn;
+	private ObjectOutputStream objOut;
+	private LoginListener listener;
+
+	/**
+	 * Create the frame.
+	 * @param objOut to communicate over socket
+	 * @param objIn to communicate over socket
+	 */
+	public LoginWindow(ObjectOutputStream objOut, ObjectInputStream objIn) {
+		
+		this.objIn = objIn;
+		this.objOut = objOut;
+		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+		listener = new LoginListener(this, objOut, objIn);
+		
+		setBounds(100, 100, 450, 300);
+		JPanel contentPane = new JPanel();
+		contentPane.setBackground(new Color(176, 196, 222));
+		contentPane.setBorder(new EmptyBorder(10, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(new BorderLayout(0, 0));
+		
+		JLabel lblLoginToDl = new JLabel("Login to D2L");
+		lblLoginToDl.setForeground(new Color(25, 25, 112));
+		lblLoginToDl.setHorizontalAlignment(SwingConstants.CENTER);
+		lblLoginToDl.setFont(new Font("PingFang TC", Font.PLAIN, 38));
+		contentPane.add(lblLoginToDl, BorderLayout.NORTH);
+		
+		JPanel loginInfo = new JPanel ();
+		loginInfo.setBackground(new Color(176, 196, 222));
+		loginInfo.setLayout(new BoxLayout(loginInfo, BoxLayout.PAGE_AXIS));
+		loginInfo.setBorder(new CompoundBorder(new EmptyBorder(40, 20, 40, 20), new MatteBorder(2, 2, 2, 2, (Color) new Color(25, 25, 112))));
+		
+		JPanel one = new JPanel ();
+		one.setBackground(new Color(176, 196, 222));
+		one.setLayout(new FlowLayout());
+		one.setBorder(new EmptyBorder(20, 0, 0, 0));
+		JLabel userLabel = new JLabel("   User ID:");
+		userLabel.setForeground(new Color(25, 25, 112));
+		userLabel.setFont(new Font("PingFang TC", Font.PLAIN, 20));
+		one.add(userLabel);
+		userID = new JTextField(15);
+		userID.setName("userID");
+		one.add(userID);
+		
+		
+		JPanel two = new JPanel ();
+		two.setBackground(new Color(176, 196, 222));
+		two.setLayout(new FlowLayout());
+		two.setBorder(new EmptyBorder(0, 20, 20, 20));
+		JLabel passLabel = new JLabel("Password:");
+		passLabel.setForeground(new Color(25, 25, 112));
+		passLabel.setFont(new Font("PingFang TC", Font.PLAIN, 20));
+		two.add(passLabel);
+		
+		passwordField = new JPasswordField(15);
+		passwordField.setSize(2, 15);
+		two.add(passwordField);
+		
+		JPanel forButton = new JPanel();
+		forButton.setBackground(new Color(176, 196, 222));
+		forButton.setLayout(new FlowLayout());
+		forButton.setBorder(new EmptyBorder(0, 20, 30, 20));
+		login = new JButton();
+		login.setForeground(new Color(25, 25, 112));
+		login.setFont(new Font("PingFang TC", Font.PLAIN, 20));
+		
+		login.setText("Login");
+		login.addActionListener(listener);
+		forButton.add(login);
+		
+		loginInfo.add(one);
+		loginInfo.add(two);
+		loginInfo.add(forButton);
+		contentPane.add(loginInfo, BorderLayout.CENTER);
+		this.pack();
+		this.setVisible(true);
+	}
+
+
+	/**
+	 * @return the login
+	 */
+	public JButton getLogin() {
+		return login;
+	}	
+	
+	/**
+	 * @return the userID
+	 */
+	public JTextField getUserID() {
+		return userID;
+	}
+	
+	/**
+	 * @return the passwordField
+	 */
+	public JPasswordField getPasswordField() {
+		return passwordField;
+	}
+	
+	/**
+	 * @param o the Object that is being communicated
+	 */
+	public void writeObject(Object o){
+		try {
+			objOut.writeObject(o);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	/**
+	 * the Object that is being communicated
+	 * @return Object read
+	 */
+	public Object readObject() {
+		try {
+			return objIn.readObject();
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+	
+}
