23f6ca3282df448015b1212808e5898f72229e2a
Shreya Patel <shreya984@hotmail.com>
2018-11-17 15:29:30 -0700

    working on login window

---
+++ PMSProject/src/model/Driver.java
@@ -0,0 +1,42 @@
+package model;
+
+import java.sql.*;
+
+/**
+ * Enables connection to the database
+ * @author Dylan Gordon, Shreya Patel, Alexa Astorino
+ *
+ */
+public class Driver {
+	protected Connection conn;
+	protected Statement stmt;
+	public String databaseName = "pms_ensf480";
+	
+	
+	public String connectionInfo = "jdbc:mysql://localhost:3306/D2LDatabase?useSSL=false",  
+			  login          = "root",
+			  password       = "HelloWorld";
+		
+	String docTable = "document", operatorTable = "operator", orderTable = "order",
+			promoTable = "promotion", regTable = "registered_buyer";
+	
+	/**
+	 * Constructor for Driver class. Enables the connection to the database.
+	 */
+	public Driver()
+	{
+		try{
+			// If this throws an error, make sure you have added the mySQL connector JAR to the project
+			Class.forName("com.mysql.jdbc.Driver");
+			
+			// If this fails make sure your connectionInfo and login/password are correct
+			conn = DriverManager.getConnection(connectionInfo, login, password);
+			System.out.println("Connected to: " + connectionInfo + "\n");
+		}
+		catch(SQLException e) { e.printStackTrace(); }
+		catch(Exception e) { e.printStackTrace(); }
+		
+	}	
+}
+
+
