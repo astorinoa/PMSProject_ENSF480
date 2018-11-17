0d097ac324ad3946591d49f6f976a8cf2ece154d
raemcphail <raemcphail@gmail.com>
2018-11-17 13:33:42 -0700

    Added Operator Class

--- PMSProject/src/control/Operator.java
+++ PMSProject/src/control/Operator.java
@@ -1,5 +1,18 @@
 package control;
-
+import java.lang.*;
 public class Operator {
-
+	private string email;
+	private string password;
+	
+	/**
+	 * Constructor for the operator
+	 * @param e is the operator's email address
+	 * @param p is the operator's password
+	 */
+	public Operator(String e, String p ) {
+		email = e;
+		password = p;
+	}
+	
+	
 }
