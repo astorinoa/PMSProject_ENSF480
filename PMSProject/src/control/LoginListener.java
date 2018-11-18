4b51245a16445bc93ddcc84c272ff8a877db646b
Alexa Astorino <alexa.astorino@gmail.com>
2018-11-18 13:12:27 -0700

    login listener complete

--- PMSProject/src/control/LoginListener.java
+++ PMSProject/src/control/LoginListener.java
@@ -39,7 +39,8 @@ public class LoginListener implements ActionListener{
 		if (a.getSource() == frame.getLogin()) {		
 			userSearch();
 		}
-		if (a.getSource() == frame.getGuest()) {		
+		if (a.getSource() == frame.getGuest()) {	
+			frame.setVisible(false);
 			BuyerAccountForm b = new BuyerAccountForm();
 			b.getCardLayout().show(b.getContentPane(), "Ordinary Home");	
 		}
