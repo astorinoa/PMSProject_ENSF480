e6f7dca1d9c42aa3bb206218f3cbf8af1e56ff40
Shreya Patel <shreya984@hotmail.com>
2018-11-22 16:51:07 -0700

    fixed serch document by id

--- PMSProject/src/control/EditDocumentController.java
+++ PMSProject/src/control/EditDocumentController.java
@@ -97,27 +97,25 @@ public class EditDocumentController extends Driver{
 	}
 	
 	
-	public ArrayList<Document> getDocumentByID(int id){
+	public Document getDocumentByID(int id){
 		String sql = "SELECT * FROM " + docTable + " WHERE DOCUMENT_ID =" + id ;
 		ResultSet document;
-		ArrayList<Document> temp = new ArrayList<Document>();
 		try {
 			stmt = conn.createStatement();
 			document = stmt.executeQuery(sql);
-			while(document.next())
+			if(document.next())
 			{
-				temp.add(new Document(document.getInt("DOCUMENT_ID"),
+				return new Document(document.getInt("DOCUMENT_ID"),
 						document.getString("TYPE"), 
 						document.getString("AUTHOR"), 
 						document.getString("TITLE"), 
 						document.getInt("PRICE"),
-						document.getInt("QUANTITY")));
+						document.getInt("QUANTITY"));
 			} 
 		}
 		catch(SQLException e) {
 			System.out.println(e.getMessage());
 		}
-		return temp;
 	}
 	
 	
